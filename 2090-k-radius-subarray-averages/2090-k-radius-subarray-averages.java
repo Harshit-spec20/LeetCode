f1156072e6214eb9653873049f27533be220e66b
Harshit-spec20 <harshitsingh22176520@gmail.com>
2026-09-08 09:55:42 +0530

    Time: 6 ms (66.23%), Space: 175.6 MB (20.81%) - LeetHub

---
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] getAverages(int[] nums, int k) {
+
+        int length = nums.length;
+        int[] result = new int[length];
+
+        int window = 2 * k + 1;
+
+        if (window > length) {
+            for (int i = 0; i < length; i++) {
+                result[i] = -1;
+            }
+            return result;
+        }
+
+        for (int i = 0; i < k; i++) {
+            result[i] = -1;
+        }
+
+        for (int i = length - k; i < length; i++) {
+            result[i] = -1;
+        }
+
+        long sum = 0;
+
+        for (int i = 0; i < window; i++) {
+            sum += nums[i];
+        }
+
+        result[k] = (int)(sum / window);
+
+        for (int i = 1; i <= length - window; i++) {
+            sum = sum - nums[i - 1] + nums[i + window - 1];
+            result[i + k] = (int)(sum / window);
+        }
+
+        return result;
+    }
+}
